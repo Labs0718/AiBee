8c2e7e7144719493b56b3db429df511fd27f3699
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2025-09-26 17:23:59 +0900

    배포환경에 맞게 포트 설정
    80 8000 둘다 접속할 수 있게 수정 (배포: 80 / 개발환경: 8000)

--- suna-main/backend/api.py
+++ suna-main/backend/api.py
@@ -253,11 +253,12 @@ if __name__ == "__main__":
     
     workers = 4
     
-    logger.info(f"Starting server on 0.0.0.0:8000 with {workers} workers")
+    port = int(os.getenv("PORT", 8000))
+    logger.info(f"Starting server on 0.0.0.0:{port} with {workers} workers")
     uvicorn.run(
-        "api:app", 
-        host="0.0.0.0", 
-        port=8000,
+        "api:app",
+        host="0.0.0.0",
+        port=port,
         workers=workers,
         loop="asyncio"
     )
