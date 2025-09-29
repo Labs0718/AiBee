8abcfe47be630331701df3bdf105eda3bd8f59f6
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2025-09-29 15:26:13 +0900

    Update layout.tsx

--- suna-main/frontend/src/app/layout.tsx
+++ suna-main/frontend/src/app/layout.tsx
@@ -66,7 +66,7 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    title: 'Suna - Open Source Generalist AI Worker',
+    title: 'AiBee - AI worker',
     description:
       'Suna is a fully open source AI assistant that helps you accomplish real-world tasks with ease through natural conversation.',
     url: siteConfig.url,
@@ -76,7 +76,7 @@ export const metadata: Metadata = {
         url: '/banner.png',
         width: 1200,
         height: 630,
-        alt: 'Suna - Open Source Generalist AI Worker',
+        alt: 'AiBee - AI worker',
         type: 'image/png',
       },
     ],
@@ -85,7 +85,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Suna - Open Source Generalist AI Worker',
+    title: 'AiBee - AI worker',
     description:
       'Suna is a fully open source AI assistant that helps you accomplish real-world tasks with ease through natural conversation.',
     creator: '@kortixai',
@@ -95,7 +95,7 @@ export const metadata: Metadata = {
         url: '/banner.png',
         width: 1200,
         height: 630,
-        alt: 'Suna - Open Source Generalist AI Worker',
+        alt: 'AiBee - AI worker',
       },
     ],
   },
